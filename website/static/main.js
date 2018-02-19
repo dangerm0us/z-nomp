a872b39af45c17f974a893eed7f9741bd4c3955e
dangerm0us <git@clique-it.com.au>
2018-02-19 16:43:54 +1100

    Site tweaks

--- website/static/main.js
+++ website/static/main.js
@@ -26,5 +26,14 @@ $(function(){
     });
 
     window.statsSource = new EventSource("/api/live_stats");
+    var aBoxclose = document.getElementsByClassName("hidealert");
+    var i;
+    for (i = 0; i < aBoxclose.length; i++) {
+        aBoxclose[i].onclick = function(){
+            var div = this.parentElement;
+            div.style.opacity = "0";
+            setTimeout(function(){ div.style.display = "none"; }, 600);
+        }
+    }
 
 });
