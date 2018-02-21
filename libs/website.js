814d52160a968be34aaafd8ce475775a6ae1c314
dangerm0us <git@clique-it.com.au>
2018-02-21 16:48:34 +1100

    Banner fixes and remove api

--- libs/website.js
+++ libs/website.js
@@ -40,7 +40,7 @@ module.exports = function(logger){
         'stats.html': 'stats',
         'tbs.html': 'tbs',
         'workers.html': 'workers',
-        'api.html': 'api',
+        // 'api.html': 'api',
         'admin.html': 'admin',
         'mining_key.html': 'mining_key',
         'miner_stats.html': 'miner_stats',
